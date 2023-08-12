8de019a66f7977cd7580049fc2dbc088f9f0fbbf
bmathieugit <bmathieugit@gmail.com>
2023-08-12 19:46:33 +0200

    add rotl

--- src/tests-ares.cpp
+++ src/tests-ares.cpp
@@ -1,6 +1,21 @@
-#include <n/string.hpp>
 #include <stdio.h>
 
+constexpr unsigned char rotl(unsigned char c, unsigned int n) {
+  unsigned int s = sizeof(c) * 8;
+  unsigned int offset = n % s;
+  return (c << offset) | (c >> (s - offset));
+}
+
+void printb(unsigned char value) {
+  for (int i = sizeof(value) * 8 - 1; i >= 0; --i) {
+    printf("%d", (value >> i) & 1);
+  }
+}
+
 int main() {
-  printf("Helloworld");  
+  printb(10);
+  printf("\n");
+  printb(rotl(10, 1));
+  printf("\n");
+  printb(rotl(10, -1));
 }
